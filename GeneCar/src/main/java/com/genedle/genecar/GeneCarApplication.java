983bdda7229e1f85154af9a0c134be7497dfd57a
Longushelen <86008239+Longushelen@users.noreply.github.com>
2022-11-04 17:13:15 +0900

    First_commit
    초기 프로젝트 생성

---
+++ GeneCar/src/main/java/com/genedle/genecar/GeneCarApplication.java
@@ -0,0 +1,13 @@
+package com.genedle.genecar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeneCarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeneCarApplication.class, args);
+	}
+
+}
