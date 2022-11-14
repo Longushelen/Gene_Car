4557ea034c5bac92c17aa909b88ee78430d49a55
HELENGOO\helen <hyejin@bizmeca.com>
2022-11-14 23:44:35 +0900

    데이터 베이스 접속하기 1

---
+++ src/main/java/genecar/config/MyBatisConfig.java
@@ -0,0 +1,33 @@
+package genecar.config;
+
+import javax.sql.DataSource;
+
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+
+@Configuration
+public class MyBatisConfig {
+
+  @Autowired
+  ApplicationContext applicationContext;
+
+  @Bean
+  public SqlSessionFactory sqlSessionFactory(DataSource dataSource) throws Exception {
+	  
+    final SqlSessionFactoryBean sessionFactory = new SqlSessionFactoryBean();
+
+    sessionFactory.setDataSource(dataSource);
+    PathMatchingResourcePatternResolver resolver = new PathMatchingResourcePatternResolver();
+    sessionFactory.setMapperLocations(resolver.getResources("classpath:mappers/*.xml"));
+    sessionFactory.setConfigLocation(
+        applicationContext.getResource("classpath:mybatis-config.xml"));
+    sessionFactory.setTypeAliasesPackage("genecar.**.vo");
+    sessionFactory.getObject().getConfiguration().setMapUnderscoreToCamelCase(true);
+    return sessionFactory.getObject();
+  }
+}
